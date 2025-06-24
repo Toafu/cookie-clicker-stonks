3bf4ddbe5fad084a2c81350b95b9650e2dca6926
Ryan Chua <toafdaloaf@gmail.com>
2025-06-24 01:28:13 -0400

    Automatically refresh on next tick

--- stonks.js
+++ stonks.js
@@ -3,13 +3,6 @@ const calculateOverhead = (numBrokers) => {
 	return 1 + 0.01 * (20 * Math.pow(0.95, numBrokers));
 };
 
-const BUYBUYBUY = 20.00;
-const stonkIDs = [...Array(18).keys()];
-const game = Game.Objects['Bank'].minigame;
-const brokers = game.brokers;
-const overhead = calculateOverhead(brokers);
-const stocks = Game.Objects['Bank'].minigame.goodsById;
-
 // Highlight low prices to buy
 const highlightLowPrices = () => {
 	game.goodsById.forEach((s, i) => {
@@ -32,9 +25,10 @@ const highlightProfits = () => {
 		if (numShares > 0) {
 			const lastBuy = numShares * s.prev * overhead;
 			const sellNow = numShares * s.val;
+			const profit = sellNow - lastBuy;
 
 			if (sellNow > lastBuy) {
-				box.style.boxShadow = '0 0 10px 2px yellow';
+				box.style.boxShadow = `0 0 10px ${Math.max(1, profit / 10000)}px yellow`;
 			} else {
 				box.style.boxShadow = '0 0 10px 2px red';
 			}
@@ -44,5 +38,26 @@ const highlightProfits = () => {
 	});
 };
 
+const BUYBUYBUY = 20.00;
+const stonkIDs = [...Array(18).keys()];
+const game = Game.Objects['Bank'].minigame;
+const brokers = game.brokers;
+const overhead = calculateOverhead(brokers);
+const stocks = Game.Objects['Bank'].minigame.goodsById;
+
 highlightLowPrices();
 highlightProfits();
+
+const nextTick = document.getElementById("bankNextTick");
+const observer = new MutationObserver(mutationList => {
+	for (let mutation of mutationList) {
+		if (mutation.type === 'childList') {
+			const nextTickSplit = nextTick.innerHTML.split(" ");
+			if (nextTickSplit.at(-2) === "1" && nextTickSplit.at(-1) === "minute.") {
+				highlightLowPrices();
+				highlightProfits();
+			}
+		}
+	}
+});
+observer.observe(nextTick, { childList: true });
